bc7f68df63f557f05e8e48b6a2c578f7ea83c54e
Raj Abhishek <138127028+Rajcommit@users.noreply.github.com>
2024-05-21 12:45:12 +0530

    Update poddelete.sh

--- poddelete.sh
+++ poddelete.sh
@@ -1,1 +1,5 @@
 kubectl get po -o wide -A | grep ip-10-70-151-204.ec2.internal | awk '{print "kubectl delete pod " $2 " -n " $1}' | sh
+
+
+kubectl scale --replicas=0 deployment/saviynt-cluster-prometheus-node-exporter-2x88c --namespace=saviynt-system
+
