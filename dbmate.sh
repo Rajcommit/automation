bc1c5ee79a3fd359bafb7efb67b02b4cccbdb469
Raj Abhishek <138127028+Rajcommit@users.noreply.github.com>
2024-05-31 13:36:45 +0530

    Update dbmate.sh

--- dbmate.sh
+++ dbmate.sh
@@ -43,4 +43,53 @@ echo "$encrypted_result"
 
 
 
+AZure:
+
+
+
+
+#!/bin/bash
+
+# this will try to get value from file
+extract_value() {
+    local file=$1
+    local var_name=$2
+    local result
+    result=$(grep "$var_name" "$file" | awk -F "=" '{print $2}' | tr -d '[:space:]"')
+    echo "$result"
+}
+
+# try to find file .groovy file and get the savcomp2 value ## <update the file system here> 
+config_file=$(find /datadrive/sharedappdrive/*/saviynt/Conf/ -name "Config.groovy")
+savcomp2=$(extract_value "$config_file" "savcomp")
+
+# try to find file .sh file and retrive savcomp3 value ## <update the file system here> 
+startup_file=$(find  /datadrive/apache/*/bin/ -name "startup.sh")
+savcomp3=$(extract_value "$startup_file" "savcomp")
+
+# Combining results in a single string
+combined_result="S@v!yn$savcomp2$savcomp3"
+
+# quotes and extra chars removed
+combined_result=$(echo "$combined_result" | tr -d '"')
+
+# encryption part
+encrypted_result=$(echo -n "$combined_result" | base64)
+
+# Printing end result
+echo "$encrypted_result"
+
+
+
+
+
+
+
+
+-  /datadrive/apache/apache-tomcat-8.5.29/bin
+					/datadrive/sharedappdrive/saviynt/Conf
+					 cd /datadrive/sharedappdrive/saviynt/logs/
+
+
+
 
